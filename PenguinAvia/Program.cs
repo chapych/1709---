ae44922527feaa99cfe6deb9f84fb958d62a6ad6
Alex Chaplygin <chapalexander@gmail.com>
2023-02-15 16:39:00 +0300

    prefinal

--- PenguinAvia/Program.cs
+++ PenguinAvia/Program.cs
@@ -1,35 +1,32 @@
-﻿
+﻿using System;
 using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
-int n   = Int32.Parse(Console.ReadLine());
-var prices = Console.ReadLine().Split(' ').Select(x=>Int32.Parse(x)).ToArray();
-var d = prices[0];
-var a = prices[1];
-var graph = new Graph(n);
-
-for (int j = n; j > 0; j--)
+public class Sum
 {
-    var line = Console.ReadLine().Split(' ').Skip(j).Select(x => Int32.Parse(x)).ToArray();
-    for (int i = 0; i < n; i++)
+    private static void Main()
     {
-        if (line[i] == 1)
-            graph.Connect(i, n - j);
+        int n = Int32.Parse(Console.ReadLine());
+        var text = new string[n];
+        var prices = Console.ReadLine().Split(' ').Select(x => Int32.Parse(x)).ToArray();
+        var d = prices[0];
+        var a = prices[1];
+        for (int i = 0; i < n; i++)
+            text[i] = Console.ReadLine();
+        var graph = Solver.FindOptimalWay(text);
+        var res = Reader.FindPrice(graph, a, d);
+        Console.WriteLine(res / 2);
+        Reader.ReadFromGraph(graph);
     }
-
 }
-var finalPrice = 0;
-var connectedList = graph.FindConnected().Select(x=>new Graph(x)); // graphs with only more than two nodes are needed
-
-foreach (var connected in connectedList)
-{
-    connected.FindCycles();
-}
-
 
 public static class Reader
 {
-    public static Graph ReadFromText(int n, params string[] text)
+    public static Graph ReadFromTextWithSpaces(params string[] text)
     {
+        var n = text.Length;
         var graph = new Graph(n);
         for (int j = 0; j<n; j++)
         {
@@ -42,9 +39,77 @@ public static class Reader
         }
         return graph;
     }
+
+    public static Graph ReadFromText(params string[] text)
+    {
+        var n = text.Length;
+        var graph = new Graph(n);
+        for (int j = 0; j < n; j++)
+        {
+            var line = text[j];
+            for (int i = j; i < n; i++)
+            {
+                if ((int)line[i] == 49)
+                    graph.Connect(i, j);
+            }
+        }
+        return graph;
+    }
+
+    public static void ReadFromGraph(Graph graph)
+    {
+        for (int i = 0; i < graph.Length; i++)
+        {
+            for (int j = 0; j < graph.Length; j++)
+            {
+                var current = graph.Boarders[i][j];
+                Console.Write(current);
+            }
+            Console.Write("\n");
+        }
+    }
+
+    public static Int64 FindPrice(Graph graph, int a, int d)
+    {
+        Int64 result = 0;
+        for (int i = 0; i < graph.Length; i++)
+        {
+            for (int j = 0; j < graph.Length; j++)
+            {
+                var current = graph.Boarders[i][j];
+                if (current == "d") result += d;
+                if (current == "a") result += a;
+            }
+        }
+        return result;
+    }
+
 }
 
+public static class Solver
+{
+    public static Graph FindOptimalWay(string[] text)
+    {
+        int n = text.Length;
+        var graph = Reader.ReadFromText(text);
+        var connectedList = graph.FindConnected().Select(x => new Graph(x)).ToList(); // graphs with only more than two nodes are needed
 
+        foreach (var connected in connectedList)
+        {
+            connected.BreakCycles();
+        }
+        var joint = connectedList.First().nodes[0];
+        for (int i = 1; i < connectedList.Count; i++)
+        {
+            var current = connectedList[i].nodes[0];
+            current.Connect(joint);
+            graph.Boarders[0][current.id] = "a";
+            graph.Boarders[current.id][0] = "a";
+        }
+
+        return graph;
+    }
+}
 public class Graph
 {
     public Node[] nodes;
@@ -53,7 +118,15 @@ public class Graph
     {
         nodes = new Node[n]; ////add nodes initialisings
         for (int i = 0; i < n; i++)
-            nodes[i] = new Node(i);
+            nodes[i] = new Node(i, this);
+        Boarders = new string[Length][];
+        for(int i = 0; i < Length; i++)
+        {
+            Boarders[i] = new string[Length];
+            for (int j = 0; j < Length; j++)
+                Boarders[i][j] = "0";
+        }
+
     }
 
     public Graph(IEnumerable<Node> values)
@@ -62,9 +135,12 @@ public class Graph
         int i = 0;
         foreach(var el in values)
             nodes[i++] = el;
+
     }
 
-    public int this[int i] => nodes[i].id;
+    public string[][] Boarders;
+    
+    public Node this[int i] => nodes[i];
     public void Connect(int a, int b)
     {
         nodes[a].Connect(nodes[b]);
@@ -77,8 +153,6 @@ public class Graph
             foreach (var node in nodes) 
                 yield return node;
         }
-
-    
     }
 
     public IEnumerable<IEnumerable<Node>> FindConnected()
@@ -89,120 +163,91 @@ public class Graph
         {
             var nextNode = this.Nodes.Where(node => !visited.Contains(node)).FirstOrDefault();
             if (nextNode == null) break;
-            var breadthSearch = this.BreadthSearch(nextNode).ToList(); 
-            //result.Add(breadthSearch.ToList());
+            var breadthSearch = BreadthSearch(nextNode).ToList(); 
+            foreach(var el in breadthSearch)
+            {
+                visited.Add(el);
+            }
             yield return breadthSearch;
         }
     }
 
-    IEnumerable<Node> BreadthSearch(Node start)
+    static IEnumerable<Node> BreadthSearch(Node start)
     {
         var visited = new HashSet<Node>();
         var queue = new Queue<Node>();
         queue.Enqueue(start);
-        while (true)
+        visited.Add(start);
+        while (queue.Count>0)
         {
             var current = queue.Dequeue();
-            foreach(var node in current.Connected)
-            {
-                queue.Enqueue(node);
-            }
-            visited.Add(current);
+            foreach (var node in current.Connected)
+                if (!visited.Contains(node))
+                {
+                    queue.Enqueue(node);
+                    visited.Add(node);
+                }
             yield return current;
         }
     }
 
-    public void FindCycles()
+    public void BreakCycles()
     {
-        var nodes = this.Nodes;
         var visited = new HashSet<Node>();  // Серые вершины
         var finished = new HashSet<Node>(); // Черные вершины
-        var nextPoints = new Stack<Node>();
-        var previousPoints = new Stack<Node>();
-        visited.Add(nodes.First());
-        nextPoints.Push(nodes.First());
-        
-        while (nextPoints.Count != 0)
+        var stack = new Stack<Node>();
+        visited.Add(this[0]);
+        stack.Push(this[0]);
+        while (stack.Count != 0)
         {
-            var node = nextPoints.Pop();
-            previousPoints.Push(node);
-            foreach (var nextNode in node.Connected)
+            var node = stack.Pop();
+            var incidentNodes = node.Connected.OrderBy(x => x.id).ToList();
+            for(int i = incidentNodes.Count-1; i>=0; i--)  
             {
+                Node nextNode = incidentNodes[i];
                 if (finished.Contains(nextNode)) continue;
                 if (visited.Contains(nextNode))
                 {
-                    var root = nextNode.Connected.Where(x => finished.Contains(x))
-                                                 .FirstOrDefault();
-                    nextNode.DisconnectExpectFor(root);
-                    DestroyCycle(previousPoints, root);
+                    node.Disconnect(nextNode);
+
                 }
                 visited.Add(nextNode);
-                nextPoints.Push(nextNode);
+                stack.Push(nextNode);
             }
-            finished.Add(node); // красим в черный, когда рассмотрели все пути из node(no inc or all inc nodes are blACK)         
-        }
-
-    }
-
-    void DestroyCycle(Stack<Node> stack, Node root)
-    {
-        while(true)
-        {
-            var current = stack.Pop();
-            if (current == null) return;////idk why could this possibly happen
-            if(current == root) return;
-            current.DisconnectExpectFor(root);
+            finished.Add(node); // красим в черный, когда рассмотрели все пути из node(no inc or all inc nodes are blACK) 
         }
     }
-
-    public int Reduce()
-    {
-        int result = 0;
-        var baseNode = Nodes.OrderByDescending(x => x.Connected.Count)
-                .FirstOrDefault();
-        if (baseNode == null) return 0;
-        foreach(var node in Nodes)
-        {
-            if (node == baseNode) continue;
-            node.DisconnectExpectFor(baseNode);
-            result++;
-        }
-        return result;
-    }
-
 }
 
 public class Node
 {
     public int id;
-    public HashSet<Node> Connected;
-
-    public Node(int id)
+    public List<Node> Connected;
+    public Graph graph;
+    public Node(int id, Graph graph)
     {
         this.id = id;
-        Connected = new HashSet<Node>();
+        Connected = new List<Node>();
+        this.graph = graph;
     }
 
     public void Connect(Node other)
     {
+        if(Connected.Contains(other)) return;
         Connected.Add(other);
         other.Connected.Add(this);
     }
 
-    public void DisconnectExpectFor(Node restNode)
+    public void Disconnect(Node other)
     {
-        Connected.Clear();
-        this.Connect(restNode);
+        Connected.Remove(other);
+        other.Connected.Remove(this);
+        graph.Boarders[this.id][other.id] = "d";
+        graph.Boarders[other.id][this.id] = "d";
     }
 
-    public override string ToString()
+    public override string ToString()   //just required for UnitTests
     {
-        var sb = new StringBuilder();
-        foreach(var el in Connected)
-            sb.Append(el.id);
-        return sb.ToString();
+        return this.id.ToString();
     }
 }
-
-
-	
--- TestProject/UnitTest1.cs
+++ TestProject/UnitTest1.cs
@@ -11,18 +11,107 @@ namespace TestProject
             {
                 var input = new string[] { "0 1", "1 0" };
                 
-                var graph = Reader.ReadFromText(input.Length, input);
-                Assert.AreEqual(graph.Length, 2);
-                Assert.AreEqual(graph.nodes[0].ToString(), "1");
+                var graph = Reader.ReadFromText(input);
+                Assert.That(graph.Length, Is.EqualTo(2));
+                Assert.That(graph.nodes[0].Connected.Select(x=>x.id), Is.EquivalentTo(new int[] { 1 }));
             }
 
             [Test]
             public void SimpleGraph_3x3()
             {
                 var input = new string[] { "0 1 0", "1 0 1", "0 1 0" };
-                var graph = Reader.ReadFromText(input.Length, input);
-                Assert.AreEqual(graph.Length, 3);
-                Assert.AreEqual(graph.nodes[1].ToString(), "02");
+                var graph = Reader.ReadFromText(input);
+                Assert.That(graph.Length, Is.EqualTo(3));
+                Assert.That(graph.nodes[1].Connected.Select(x => x.id), Is.EquivalentTo(new int[] { 2,0 }));
+            }
+
+            [Test]
+            public void SimpleGraph_Empty()
+            {
+                var input = new string[] { "0 0 0", "0 0 0", "0 0 0" };
+                var graph = Reader.ReadFromText(input);
+                Assert.That(graph.Length, Is.EqualTo(3));
+                Assert.That(graph.nodes[1].Connected.Select(x => x.id), Is.EquivalentTo(new int[] {}));
+            }
+        }
+
+        [TestFixture]
+        public class CorrectConnectedSubGraphs
+        {
+            [Test]
+            public void NonConnectedGraph()
+            {
+                var input = new string[] { "0 0 0", "0 0 0", "0 0 0" };
+                var graph = Reader.ReadFromText(input);
+                var actual = graph.FindConnected().Select(x => x.Select(node => node.id).ToArray()).ToList();
+                int i = 0;
+                foreach(var array in actual)
+                {
+                    Assert.That(array, Is.EquivalentTo(new int[] {i++}));
+                }
+            }
+
+            [Test]
+            public void ConnectedGraph1()
+            {
+                var input = new string[] { "0 1 1", "1 0 1", "1 1 0" };
+                var graph = Reader.ReadFromText(input);
+                var actual = graph.FindConnected().Select(x => x.Select(node => node.id).ToArray()).ToList();
+                Assert.That(actual[0], Is.EquivalentTo(new int[] {0,2,1 }));
+                
+            }
+
+            [Test]
+            public void ConnectedGraph2()
+            {
+                var input = new string[] { "0 0 1 0", "0 0 0 1", "1 0 0 0", "0 1 0 0" };
+                var graph = Reader.ReadFromText(input);
+                var actual = graph.FindConnected().Select(x => x.Select(node => node.id).ToArray()).ToList();
+                Assert.That(actual[0], Is.EquivalentTo(new int[] { 0, 2 }));
+
+            }
+
+
+
+        }
+
+        [TestFixture]
+        public class CorrectDestroyingACycle
+        {
+            
+
+            [Test]
+            public void TransformFromCycleToTree()
+            {
+                var input = new string[] { "0 1 0 0 0 0 1", "1 0 1 0 0 0 0", "0 1 0 1 0 0 0", "0 0 1 0 1 0 0", "0 0 0 1 0 1 0", "0 0 0 0 1 0 1", "1 0 0 0 0 1 0" };
+                var graph = Reader.ReadFromText(input);
+                graph.BreakCycles();
+                Assert.That(graph.nodes[1].Connected.Select(x => x.id), Is.EquivalentTo(new int[] { 0,2 }));
+                Assert.That(graph.nodes[5].Connected.Select(x => x.id), Is.EquivalentTo(new int[] { 4}));
+            }
+        }
+
+        [TestFixture]
+        public class CorrectRoad
+        {
+            [Test]
+            public void Road1()
+            {
+                var input = new string[] { "0 1 1 0 0", "1 0 1 0 0", "1 1 0 0 0", "0 0 0 0 1", "0 0 0 1 0" };
+
+                var graph = Solver.FindOptimalWay(input);
+                Assert.That(graph.nodes[0].Connected.Select(x => x.id), Is.EquivalentTo(new int[] { 1,2,3 }));
+                Assert.That(graph.nodes[4].Connected.Select(x => x.id), Is.EquivalentTo(new int[] {  3 }));
+
+            }
+
+            [Test]
+            public void Road2()
+            {
+                var input = new string[] { "0 1 1 0 0 0", "1 0 1 0 0 0", "1 1 0 0 0 0", "0 0 0 0 1 1", "0 0 0 1 0 1", "0 0 0 1 1 0" };
+                var graph = Solver.FindOptimalWay(input);
+                Assert.That(graph.nodes[0].Connected.Select(x => x.id), Is.EquivalentTo(new int[] {  2,1,3 }));
+                Assert.That(graph.nodes[4].Connected.Select(x => x.id), Is.EquivalentTo(new int[] { 3 }));
             }
         }
     }
